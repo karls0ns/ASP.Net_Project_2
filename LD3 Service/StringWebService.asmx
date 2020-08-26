740e8a564f7cc28f6854789c6ba9c88f5f26d528
karls0ns <62147719+karls0ns@users.noreply.github.com>
2020-08-26 11:30:49 +0300

    Add files via upload

---
+++ LD3 Service/StringWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="StringWebService.asmx.cs" Class="LD3_Service.StringWebService" %>
---
+++ LD3 Service/StringWebService.asmx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace LD3_Service
+{
+    /// <summary>
+    /// Summary description for StringWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class StringWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string Edit(string txt)
+        {
+            string work = txt;
+            
+            //Lieko tukšumu likvidēšana
+            string new_st = "";
+            for (int j = 0; j < work.Length; j++)
+            {
+                if (work.Substring(j, 1) == " ")
+                {
+                    if (j < work.Length - 1 && work.Substring(j + 1, 1) != " ")
+                    {
+                        new_st += work.Substring(j, 1);
+                    }
+                }
+                else                    
+                {
+                    new_st += work.Substring(j, 1);
+                }
+            }
+            work = new_st.Trim();
+
+            //Tukšumu pirms . un , likvidēšana
+            new_st = "";
+            for (int j = 0; j < work.Length; j++)
+            {
+                if (work.Substring(j, 1) == " ")
+                {
+                    if (j < work.Length - 1 && (work.Substring(j + 1, 1) == "," || work.Substring(j + 1, 1) == "."))
+                    {
+                            
+                    }
+                    else
+                    {
+                        new_st += work.Substring(j, 1);
+                    }
+                }
+                else
+                {
+                    new_st += work.Substring(j, 1);
+                }
+            }
+            work = new_st.Trim();
+
+            //Tukšumu pēc . un , ieviešana
+            new_st = "";
+            for (int j = 0; j < work.Length; j++)
+            {
+                if (work.Substring(j, 1) == ","|| work.Substring(j, 1) == ".")
+                {
+                    if (j < work.Length - 1 && work.Substring(j + 1, 1) != " ")
+                    {
+                        new_st += work.Substring(j, 1);
+                        new_st += " ";
+                    }
+                    else
+                    {
+                        new_st += work.Substring(j, 1);
+                    }
+                }
+                else
+                {
+                    new_st += work.Substring(j, 1);
+                }
+            }
+            work = new_st.Trim();
+            return work;            
+        }
+    }
+}
